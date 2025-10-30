2380bbad6dbb8901ab8199f272c28b88e16e166c
theo <crafty.figures@gmail.com>
2025-10-30 15:24:01 +0100

    fix routing

--- src/routing.ts
+++ src/routing.ts
@@ -1,20 +1,16 @@
 import path from "path";
 import { DependsOnMethod, Routing, ServeStatic } from "express-zod-api";
 import { ListRecipeEndpoint } from "../modules/recipe/endpoints/ListRecipe.ts";
-<<<<<<< HEAD
 import { CreateRecipeEndPoint } from "$modules/recipe/endpoints/CreateRecipe.ts";
 import { DeleteRecipeEndPoint } from "$modules/recipe/endpoints/DeleteRecipe.ts";
-import {ListMenusEndpoint} from "$modules/menu/endpoints/ListMenus";
+import { ListMenusEndpoint } from "$modules/menu/endpoints/ListMenus.ts";
+import { CreateMenuEndpoint } from "$modules/menu/endpoints/CreateMenu.ts";
 import {ListIngredientsEndpoint} from "$modules/ingredient/endpoints/ListIngredient";
 import {CreateIngredientEndpoint} from "$modules/ingredient/endpoints/CreateIngredient";
 import {DeleteIngredientEndpoint} from "$modules/ingredient/endpoints/DeleteIngredient";
 import { UpdateIngredientEndpoint } from "$modules/ingredient/endpoints/UpdateIngredient";
 import { GetIngredientByIdEndpoint } from "$modules/ingredient/endpoints/GetIngredientByIdEndpoint";
 import { SearchIngredientEndpoint } from "$modules/ingredient/endpoints/SearchIngredient";
-=======
-import { CreateRecipeEndPoint } from "../modules/recipe/endpoints/CreateRecipe.ts";
-import { ListIngredientEndpoint } from "../modules/ingredient/endpoints/ListIngredient.ts";
->>>>>>> f0042e960c87c5d309fb746a9b8d6f52bef59228
 
 export const routing: Routing = {
   v1: {
@@ -23,10 +19,10 @@ export const routing: Routing = {
       post: CreateRecipeEndPoint,
       delete: DeleteRecipeEndPoint,
     }),
-<<<<<<< HEAD
-    menu: {
-      "/": ListMenusEndpoint
-    },
+    menus: new DependsOnMethod({
+      get: ListMenusEndpoint,
+      post: CreateMenuEndpoint,
+    }),
     ingredients: {
       "/": new DependsOnMethod({
         get: ListIngredientsEndpoint,
@@ -39,11 +35,7 @@ export const routing: Routing = {
         get: GetIngredientByIdEndpoint,
         put: UpdateIngredientEndpoint,
         delete: DeleteIngredientEndpoint,
-      }),
-=======
-    ingredient: {
-      "/": ListIngredientEndpoint
->>>>>>> f0042e960c87c5d309fb746a9b8d6f52bef59228
+      })
     },
   },
   public: new ServeStatic(path.join(__dirname, "../assets"), {
@@ -52,4 +44,3 @@ export const routing: Routing = {
     redirect: false,
   }),
 };
-
